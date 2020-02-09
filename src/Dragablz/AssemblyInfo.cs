7eb5b90f3503f627c9427630a79e4902b51565bd
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2020-02-09 17:04:28 -0500

    Initial import

---
+++ src/Dragablz/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo ( ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly )]
+
+[assembly: XmlnsPrefix     ( "http://dragablz.net/winfx/xaml/dragablz", "dragablz" )]
+[assembly: XmlnsDefinition ( "http://dragablz.net/winfx/xaml/dragablz", "Dragablz" )]
+[assembly: XmlnsPrefix     ( "http://dragablz.net/winfx/xaml/dockablz", "dockablz" )]
+[assembly: XmlnsDefinition ( "http://dragablz.net/winfx/xaml/dockablz", "Dragablz.Dockablz" )]
